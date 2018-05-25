4748aac18519aadbad5bc14771a37e59dbaf3688
Liugan <liugan1996@foxmail.com>
2018-05-25 22:44:36 +0800

    add: 用户控制层类

---
+++ blog/src/main/java/controller/UserController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author liugan83@gmail.com
+ * @version V1.0
+ * @date 2018/5/25 0025 22:43
+ * @Description 控制层
+ */
+@Controller
+@RequestMapping("/admin")
+public class UserController {
+
+}
