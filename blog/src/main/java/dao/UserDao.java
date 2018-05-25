933f6080d252fe1da397c0c0d3829f6a37b46e17
Liugan <liugan1996@foxmail.com>
2018-05-25 22:44:05 +0800

    add: 用户持久层类

---
+++ blog/src/main/java/dao/UserDao.java
@@ -0,0 +1,26 @@
+package dao;
+
+import entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author liugan83@gmail.com
+ * @version V1.0
+ * @date 2018/5/25 0025 22:26
+ * @Description 用户操作持久层
+ */
+@Repository
+public interface UserDao extends JpaRepository<User,String> {
+
+	/**
+	 * 根据用户名密码查找用户
+	 * @param username 用户名
+	 * @param password 密码
+	 * @return	匹配到的user对象
+	 */
+	@Query("select u from User u where u.username = :username and password = :password")
+	User findByUsernameAndPassword(@Param("username") String username,@Param("password") String password);
+}
