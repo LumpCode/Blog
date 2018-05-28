9bc7cc41914f034fd50566e41f83220870ca8912
Liugan <liugan1996@foxmail.com>
2018-05-28 14:19:37 +0800

    add: 入口函数

---
+++ blog/src/main/java/Application.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liugan83@gmail.com
+ * @version V1.0
+ * @date 2018/5/28 0028 13:42
+ * @Description 入口
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class,args);
+	}
+}
